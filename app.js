fc1a55e2568b9130cad0a0571d81c2ab0dd38150
Alexander Shuvalov <ashuv_vyksa@mail.ru>
2020-06-10 19:01:59 +0300

    add port

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ app.get('/', function (req, res) {
   res.send('<h1>Express Travis Heroku</h1>');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
 })
